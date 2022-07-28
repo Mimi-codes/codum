dae36b145a01699e4ca7db5316baca5fcc6b170f
Tomi <mariamismael904@gmail.com>
2022-07-28 16:50:21 +0100

    ...

---
+++ build/precache-manifest.03cfa9e3a2ef40afd6963f78bc7893c4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd4a05e380da5d275466",
+    "url": "/static/css/main.1e560541.chunk.css"
+  },
+  {
+    "revision": "dd4a05e380da5d275466",
+    "url": "/static/js/main.dd4a05e3.chunk.js"
+  },
+  {
+    "revision": "655ced442bf892c5e859",
+    "url": "/static/js/1.655ced44.chunk.js"
+  },
+  {
+    "revision": "f062c54403a75fd802ed",
+    "url": "/static/js/2.f062c544.chunk.js"
+  },
+  {
+    "revision": "8610012c5615a631c0bc",
+    "url": "/static/js/runtime~main.8610012c.js"
+  },
+  {
+    "revision": "011365837009ddf81fbb066d2596abce",
+    "url": "/static/media/s3.01136583.jpg"
+  },
+  {
+    "revision": "ee8cf4109719a36407bb7827266c60c9",
+    "url": "/static/media/chair.ee8cf410.png"
+  },
+  {
+    "revision": "2b0dfc1c5ce6be19cef60811da228a30",
+    "url": "/static/media/led.2b0dfc1c.jpg"
+  },
+  {
+    "revision": "1ce3704b2602c8bea5437169edf1e688",
+    "url": "/static/media/fur.1ce3704b.jpg"
+  },
+  {
+    "revision": "844d44c02a83f90947bf7862bc0bea12",
+    "url": "/static/media/edited.844d44c0.png"
+  },
+  {
+    "revision": "c13a1100ced539cf687d007ae227a99a",
+    "url": "/static/media/comfy2.c13a1100.jpg"
+  },
+  {
+    "revision": "20130369b64ccb966830e3f7cb21a034",
+    "url": "/static/media/couch2.20130369.jpg"
+  },
+  {
+    "revision": "bd3d8c8487a551dbafbdc396bcac95b9",
+    "url": "/static/media/sofa.bd3d8c84.jpg"
+  },
+  {
+    "revision": "0faab9c439dabc6b46da79725cfff120",
+    "url": "/static/media/guest.0faab9c4.jpg"
+  },
+  {
+    "revision": "a81b54dbb3afa72ddaa15e3ca16a6cbf",
+    "url": "/static/media/870.a81b54db.jpg"
+  },
+  {
+    "revision": "936d17413ae3197948b74b079b05ce26",
+    "url": "/index.html"
+  }
+];
